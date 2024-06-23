ffbfe8d84a227052869acc9ef51de153efd0ca04
Marcos <marcosraach.1@gmail.com>
2024-06-23 02:41:08 -0300

    feat: Add game mode selection and implement computer moves

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { createInterface } from 'readline'
 import { board, printBoard, resetBoard } from './board.js'
+import { minimax } from './bot.js'
+import { getWinner } from './utils.js'
 
 let currentPlayer = 'X'
 let playerXName
 let playerOName
+let gameMode
 
 const rl = createInterface({
     input: process.stdin,
@@ -11,19 +14,41 @@ const rl = createInterface({
 })
 
 function promptPlayerNames() {
-    rl.question('Digite o seu nome 1 Jogador: ', nameX => {
-        playerXName = nameX.trim()
-        rl.question('Digite o seu nome 2 Jogador: ', nameO => {
-            playerOName = nameO.trim()
-            promptMove()
-        })
+    rl.question('Escolha o modo de jogo:\n1 - Contra outro jogador\n2 - Contra o computador\n', (mode) => {
+        gameMode = mode.trim()
+
+        if (mode.trim() === '1') {
+            rl.question('Digite o seu nome 1 Jogador: ', nameX => {
+                playerXName = nameX.trim()
+                rl.question('Digite o seu nome 2 Jogador: ', nameO => {
+                    playerOName = nameO.trim()
+                    promptMove()
+                })
+            })
+        }
+
+        if (mode.trim() === '2') {
+            rl.question('Digite o seu nome Jogador: ', nameX => {
+                playerXName = nameX.trim()
+                playerOName = 'Computador'
+                promptMove()
+            })
+        }
+
+        if (mode.trim() !== '1' && mode.trim() !== '2') {
+            console.log('Escolha inválida. Por favor, escolha 1 ou 2.')
+            promptPlayerNames()
+        }
     })
 }
 
 function promptMove() {
     printBoard()
-    rl.question(`Jogador ${currentPlayer === 'X' ? playerXName : playerOName}, insira sua jogada (linha e coluna): `, input => {
-        const trimmedInput = input.trim();
+    if (isBotTurn()) {
+        performBotMove()
+    }
+    rl.question(`\n Jogador ${currentPlayer === 'X' ? playerXName : playerOName}, insira sua jogada (linha e coluna): `, input => {
+        const trimmedInput = input.trim()
         const inputs = trimmedInput.split(' ').map(Number)
         if (inputs.length === 2) {
             const [row, col] = inputs
@@ -41,13 +66,12 @@ function promptMove() {
 
 function makeMove(row, col) {
     if (board[row][col] !== ' ') {
-        console.log('\n Posicao no board já preenchida, escolha outra posicao! \n')
-        return false
+        return console.log('\n Posicao no board já preenchida, escolha outra posicao! \n')
     }
 
     board[row][col] = currentPlayer
 
-    if (checkWin(currentPlayer)) {
+    if (getWinner(board) === currentPlayer) {
         printBoard()
         console.log(`Jogador ${currentPlayer === 'X' ? playerXName : playerOName} venceu!`)
         return true
@@ -60,22 +84,10 @@ function makeMove(row, col) {
     }
 
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
-    return false
-}
 
-function checkWin(player) {
-    for (let row of board) {
-        if (row.every(cell => cell === player)) return true
+    if (!isBotTurn()) {
+        promptMove()
     }
-
-    for (let col = 0; col < 3; col++) {
-        if (board[0][col] === player && board[1][col] === player && board[2][col] === player) return true
-    }
-
-    if (board[0][0] === player && board[1][1] === player && board[2][2] === player) return true
-    if (board[0][2] === player && board[1][1] === player && board[2][0] === player) return true
-
-    return false
 }
 
 function checkDraw() {
@@ -83,14 +95,33 @@ function checkDraw() {
 }
 
 function resetGame() {
-    rl.question('Vocês desejam jogar novamente? (s/n) ', (answer) => {
+    if (gameMode === 1) {
+        rl.question('Vocês desejam jogar novamente? (s/n) ', (answer) => {
+            if (answer.trim().toLowerCase() === 's') {
+                resetBoard()
+                currentPlayer = 'X'
+                return promptMove()
+            }
+            rl.close()
+        })
+    }
+    rl.question('Você deseja jogar novamente? (s/n) ', (answer) => {
         if (answer.trim().toLowerCase() === 's') {
             resetBoard()
             currentPlayer = 'X'
             return promptMove()
-        } 
+        }
         rl.close()
     })
+}
+
+function isBotTurn() {
+    return gameMode === '2' && currentPlayer === 'O'
+}
+
+function performBotMove() {
+    const bestMove = minimax(board, 'O')
+    makeMove(Math.floor(bestMove.index / 3), bestMove.index % 3)
 }
 
 promptPlayerNames()
